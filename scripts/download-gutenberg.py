cf352c48eaca50bc01142e037242b38d822254f2
Julian <julian24816@gmail.com>
2024-10-23 14:42:52 +0200

    Refactor and enhance Gutenberg books downloader
    Refactored `parse_rdf` to include book_id and added utility to handle integer conversions safely. Introduced `download_metadata_and_text` function to streamline the download and save process. Enhanced main script to download and process multiple books efficiently, including a check for existing metadata files.

--- scripts/download-gutenberg.py
+++ scripts/download-gutenberg.py
@@ -6,6 +6,7 @@ from typing import List
 import requests
 import yaml
 
+
 @dataclass
 class Agent:
     name: str
@@ -14,12 +15,14 @@ class Agent:
     aliases: List[str]
     webpage: str
 
+
 @dataclass
 class FileFormat:
     about: str
     extents: List[int]
     modified: List[str]
     formats: List[str]
+
 
 @dataclass
 class GutenbergBook:
@@ -46,6 +49,7 @@ def download_rdf(book_id: int, directory: Path) -> Path:
         f.write(r.content)
     return path
 
+
 def download_text(book_id: int, directory: Path) -> Path:
     path = directory / f"{book_id}.txt"
     r = requests.get(f"https://www.gutenberg.org/cache/epub/{book_id}/pg{book_id}.txt.utf8")
@@ -55,7 +59,7 @@ def download_text(book_id: int, directory: Path) -> Path:
     return path
 
 
-def parse_rdf(path: Path) -> GutenbergBook:
+def parse_rdf(path: Path, book_id: int) -> GutenbergBook:
     g = Graph()
     g.parse(path)
 
@@ -69,19 +73,27 @@ def parse_rdf(path: Path) -> GutenbergBook:
         resource = g.value(subject, predicate)
         return resource.toPython() if resource else None
 
-    ebook = URIRef("http://www.gutenberg.org/ebooks/100")
+    def try_get_int(value: str, else_result: int = 0) -> int:
+        try:
+            return int(value)
+        except TypeError:
+            return else_result
+        except ValueError:
+            return else_result
+
+    ebook = URIRef(f"http://www.gutenberg.org/ebooks/{book_id}")
 
     publisher = get_text(ebook, DCTERMS.publisher)
     license = get_resource(ebook, DCTERMS.license)
     issued = get_text(ebook, DCTERMS.issued)
     rights = get_text(ebook, DCTERMS.rights)
-    downloads = int(get_text(ebook, PGTERMS.downloads))
+    downloads = try_get_int(get_text(ebook, PGTERMS.downloads))
 
     creator_node = g.value(ebook, DCTERMS.creator)
     creator = Agent(
         name=get_text(creator_node, PGTERMS.name),
-        birthdate=int(get_text(creator_node, PGTERMS.birthdate)),
-        deathdate=int(get_text(creator_node, PGTERMS.deathdate)),
+        birthdate=try_get_int(get_text(creator_node, PGTERMS.birthdate)),
+        deathdate=try_get_int(get_text(creator_node, PGTERMS.deathdate)),
         aliases=[alias.toPython() for alias in g.objects(creator_node, PGTERMS.alias)],
         webpage=get_resource(creator_node, PGTERMS.webpage)
     )
@@ -122,7 +134,6 @@ def parse_rdf(path: Path) -> GutenbergBook:
         )
         hasFormats.append(hasFormat)
 
-
     return GutenbergBook(
         publisher=publisher,
         license=license,
@@ -139,16 +150,81 @@ def parse_rdf(path: Path) -> GutenbergBook:
         hasFormats=hasFormats
     )
 
+
 def save_as_yaml(book: GutenbergBook, directory: Path, book_id: int) -> Path:
     yaml_filepath = directory / f'{book_id}.yaml'
     with open(yaml_filepath, 'w', encoding='utf-8') as yaml_file:
         yaml.dump(asdict(book), yaml_file, default_flow_style=False, allow_unicode=True)
     return yaml_filepath
 
+
+def download_metadata_and_text(book_id: int, directory: Path) -> GutenbergBook:
+    rdf_path = download_rdf(book_id, directory)
+    download_text(book_id, directory)
+    metadata = parse_rdf(rdf_path, book_id)
+    save_as_yaml(metadata, directory, book_id)
+    return metadata
+
+
 if __name__ == "__main__":
     gut_dir = Path("data/gutenberg")
     gut_dir.mkdir(parents=True, exist_ok=True)
-    rdf_path = download_rdf(100, gut_dir)
-    text_path = download_text(100, gut_dir)
-    the_book = parse_rdf(rdf_path)
-    save_as_yaml(the_book, gut_dir, 100)
+    for b in (
+            35,  # The Time Machine
+            36,  # The War of the Worlds
+            41,  # The Legend of Sleepy Hollow
+            43,  # The Strange Case of Dr. Jekyll and Mr. Hyde
+            62,  # A Princess of Mars
+            100,  # collected works of shakespeare
+            135,  # Les Misérables
+            209,  # The Turn of the Screw
+            215,  # The Call of the Wild
+            696,  # The Castle of Otranto
+            768,  # Wuthering Heights
+            996,  # Don Quixote
+            1184,  # The Count of Monte Cristo
+            1661,  # The Adventures of Sherlock Holmes
+            1666,  # The Golden Asse
+            2701,  # Moby Dick
+            1251,  # Le Morte d’Arthur (Vol 1)
+            1952,  # The Yellow Wallpaper
+            11000,  # An Old Babylonian Version of the Gilgamesh Epic
+            13268,  # Hindu literature : Comprising The Book of good counsels, Nala and Damayanti, The Ramayana, and Sakoontala
+            16328,  # Beowulf
+            19033,  # Alice's Adventures in Wonderland
+            19942,  # Candide
+            21765,  # Metamorphoses (1-7)
+            22120,  # The Canterbury Tales
+            22456,  # The Aeneid
+            23700,  # The Decameron
+            26740,  # The Picture of Dorian Gray
+            34206,  # The Thousand and One Nights (Vol 1)
+            42324,  # frankenstein
+            42537,  # The Divine Comedy
+            42671,  # pride and prejudice
+            43936,  # The Wonderful Wizard of Oz
+            45839,  # Dracula
+            49487,  # Anna Karenina
+            58585,  # The Prophet
+            58866,  # The Murder on the Links
+            59254,  # The Inimitable Jeeves
+            59306,  # The Iliad & The Odyssey
+            59437,  # A Son at the Front
+            59794,  # The World Crisis
+            61077,  # The King of Elfland's Daughter
+            61221,  # A Passage to India
+            64317,  # The Great Gatsby
+            65473,  # Gulliver's Travels
+            66057,  # The Tale of Genji
+            67098,  # Winnie-the-Pooh
+            67138,  # The Sun Also Rises
+            68283,  # The Call of Cthulhu
+            70271,  # When We Were Very Young
+            70841,  # Robinson Crusoe
+            70875,  # Arrowsmith
+            71865,  # Mrs. Dalloway
+    ):
+        if (gut_dir / f"{b}.yaml").exists():
+            continue
+        meta = download_metadata_and_text(b, gut_dir)
+        print("downloaded", b, meta.title)
